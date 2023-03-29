faac8d2765ab546a6b11a4cf7f7b4ce43315f5df
IAmARodent <geronimo.aldana0@gmail.com>
2023-03-29 15:58:51 -0700

    signup and login API requests

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 app.get('/login', (request, response) =>{
-    response.json('test');
+    console.log(request.body);
+    response.json('Login GET request is through');
+});
+
+app.post('/signup', (request, response) =>{
+    console.log(request.body);
+    response.json('Sign-up POST request is through');
 });
 
 app.listen(3001);
