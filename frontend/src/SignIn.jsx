93438f9e4055709d9a365ef7672504709a6d8fac
Josh Ebreo <61673073+Josh-Ebreo@users.noreply.github.com>
2023-03-30 17:28:50 -0700

    Add files via upload

---
+++ frontend/src/SignIn.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import SnoozeHeader from './general/SnoozeHeader'
+
+const SignIn = (props) => {
+  return (
+    <>
+      <div className="sign-in-container">
+        <div className="sign-in-sign-in-web">
+          <span className="sign-in-text">
+            <span>Sign In</span>
+          </span>
+      </div>
+      </div>
+    </>
+  )
+}
+
+export default SignIn
