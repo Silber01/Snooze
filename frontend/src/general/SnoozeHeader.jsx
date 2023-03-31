fe8f3efce34a1ae86ee54f3ba08494ad3642d5bc
Josh Ebreo <61673073+Josh-Ebreo@users.noreply.github.com>
2023-03-30 17:24:58 -0700

    Add files via upload

---
+++ frontend/src/general/SnoozeHeader.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./SnoozeHeader.css"
+
+
+function SnoozeHeader()
+{
+    return (
+        <div className="SnoozeHeader">
+            <img src='../../assets/SnoozeLogo.svg'></img>
+
+        </div>
+
+    )
+}
+
+
+export default SnoozeHeader
