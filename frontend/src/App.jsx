1971f8576c538b142f42745589cc25bbc34224f6
Daniel Slade <dangslade@gmail.com>
2023-04-18 18:11:29 -0700

    adds payment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,19 +6,18 @@ import EditProfile from './pages/EditProfile';
 import SignUp from './sign/SignUp';
 import ForgotPw from './pages/ForgotPw';
 import EditPassword from './pages/EditPassword';
-import ProfilePage from './pages/ProfilePage';
 
 function App() {
-  let userData = JSON.parse(localStorage.getItem('user'))
-  if (!userData)
-  { 
-    userData = {
-      // testing for profile page
-      firstName: "Daniel",
-      lastName: "Slade",
-      email: "test@gmail.com",
-    }
-  }
+  // for frontend testing only, will connect with backend and change these lines later
+
+  // email_logged_in = JSON.parse(localStorage.getItem('user')).email
+  // if (email_logged_in == null) {
+  //   email_logged_in = 'daniel@gmail.com'
+  // } 
+  // if you go to edit profile without logging in, the app will break and you will need to manually set "email" and run it, 
+  // then change it back to JSON.parse(localStorage.getItem('user')).email
+  // the code above is what i tried to do to fix it, but it didn't work
+  // - Connor
   
   const user = {
     firstName: userData.firstName,
@@ -35,6 +34,7 @@ function App() {
             <Route exact path = "/editprofile" element={<EditProfile user={user}/>}/>
             <Route exact path = "/forgotpassword" element={<ForgotPw/>}/>
             <Route exact path = "/editpassword" element={<EditPassword/>}/>
+            <Route exact path = "/payment" element={<Payment user={user} />}/>
         </Routes>
       </BrowserRouter>
     </div>
