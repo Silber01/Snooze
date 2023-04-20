6b81c5dd33c0da57853439360abf9f699b399bc0
Connor Bustos <32989507+connorbustos@users.noreply.github.com>
2023-04-20 14:59:35 -0700

    added navbar.jsx and added chakra into the Homepage.jsx
    looks like this rn: https://media.discordapp.net/attachments/1070198283512848426/1098729530769805402/image.png?width=950&height=505

--- frontend/src/home/HomePage.jsx
+++ frontend/src/home/HomePage.jsx
@@ -1,88 +1,99 @@
-import React, { useState, useRef, useEffect } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import sampleHotelData from "../../sampleHotels.json"
-import SnoozeHeader from '../general/SnoozeHeader'
-import SearchBar from './SearchBar';
-import './HomePage.css';
-import {GrLocation} from 'react-icons/gr' //npm install react-icons --save
-import {MdDateRange} from 'react-icons/md'
-import {MdPeopleOutline} from 'react-icons/md'
-import {BiSearch} from 'react-icons/bi'
-import HotelFilter from "./HotelFilter";
+import React, { useRef } from "react";
+import sampleHotelData from "../../sampleHotels.json";
+import NavBar from "../general/Navbar";
+import "./HomePage.css";
+
+import { Box, Flex, Button, Input } from "@chakra-ui/react";
 
 function HomePage(props) {
-    let hotels = sampleHotelData.HOTELS  
-    const searchRef = useRef();
-    return (
-        <div>
-            <SnoozeHeader />
-            <div className='searchBarWrapper'>
-                <div className='SearchBarContainer'>
-                    <SearchBar placeholder="Location">
-                        <GrLocation className='searchbar-icon'/>
-                    </SearchBar >
-                    <SearchBar placeholder="Check in - out">
-                        <MdDateRange className='searchbar-icon'/>
-                    </SearchBar >
-                    <SearchBar placeholder="Guest & Rooms">
-                        <MdPeopleOutline className='searchbar-icon'/>
-                    </SearchBar >
-                    <button className='searchButton' type="submit" onClick={() => {
-                            (searchRef)
-                        }}>
-                        Search
-                    </button>
-                </div>
-            </div>
-            <HotelFilter/>
+  let hotels = sampleHotelData.HOTELS;
+  const searchRef = useRef();
 
-            <div className='BannerContainer'>
-                <h1>Wanna Earn Rewards? Sign Up Today and Begin Collecting Points!</h1>
-            </div>
-            
-            
+  const numSquares = 20;
+  // create an array of empty squares to display as placeholders
+  const emptySquares = Array.from(Array(numSquares)).map((_, index) => (
+    <Box
+      key={index}
+      w={["90vw", "80vw", "400px"]}
+      h={["90vw", "80vw", "400px"]}
+      bg="gray.200"
+      m="3"
+    />
+  ));
+
+  return (
+    <div>
+      <NavBar />
+      <div className="searchBarWrapper">
+        <div className="SearchBarContainer">
+          Search for a Hotel
+          <Flex>
+            <Input
+              size="md"
+              variant="filled"
+              placeholder="Location"
+              marginRight={10}
+            />
+            <Input
+              size="md"
+              variant="filled"
+              placeholder="Check In Date"
+              marginRight={10}
+            />
+            <Input
+              size="md"
+              variant="filled"
+              placeholder="Check Out Date"
+              marginRight={10}
+            />
+            <Button
+              colorScheme="green"
+              type="submit"
+              onClick={() => {
+                searchRef;
+              }}
+              width="40%"
+            >
+              Search
+            </Button>
+          </Flex>
         </div>
-        
-        
-    
-    )
+      </div>
+      {/* <HotelFilter /> */}
+      {/*render all hotels*/}
+      <Flex flexWrap="wrap" justifyContent="center" alignItems="center" mt="8">
+        {emptySquares}
+      </Flex>
+    </div>
+  );
 }
 
-
-function printHotelRoom(hotel)
-
-    {
-        function hasWifiText(hasWifi)
-        {
-            if (hasWifi)
-                return "Has Free Wifi"
-            else
-                return "No Free Wifi"
-        }
+function printHotelRoom(hotel) {
+  function hasWifiText(hasWifi) {
+    if (hasWifi) return "Has Free Wifi";
+    else return "No Free Wifi";
+  }
+  return (
+    <>
+      <div>{hotel.name}</div>
+      <div>{hotel.description}</div>
+      <div>{hotel.location.address}</div>
+      <div>{hotel.location.city}</div>
+      <div>{hotel.location.province}</div>
+      <div>{hotel.location.country}</div>
+      <div>ROOMS:</div>
+      {hotel.rooms.map((room, i) => {
         return (
-            <>
-            <div>{hotel.name}</div>
-            <div>{hotel.description}</div>
-            <div>{hotel.location.address}</div>
-            <div>{hotel.location.city}</div>
-            <div>{hotel.location.province}</div>
-            <div>{hotel.location.country}</div>
-            <div>ROOMS:</div>
-            {
-                hotel.rooms.map((room, i) => {
-                    return (
-                        <>
-                            <div>{room.name}</div>
-                            <div>${room.price}</div>
-                            <div>{room.beds} bed </div>
-                            <div>{hasWifiText(room.hasWifi)}</div>
-                        </>
-                    )
-                })
-            }
-            </>
-        )
-    }
+          <>
+            <div>{room.name}</div>
+            <div>${room.price}</div>
+            <div>{room.beds} bed </div>
+            <div>{hasWifiText(room.hasWifi)}</div>
+          </>
+        );
+      })}
+    </>
+  );
+}
 
-
-export default HomePage
+export default HomePage;
