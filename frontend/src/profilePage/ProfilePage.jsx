444d6856c48bbd12940558939177e817edd90516
Connor Bustos <32989507+connorbustos@users.noreply.github.com>
2023-04-25 21:08:24 -0700

    added hotel not found component

--- frontend/src/profilePage/ProfilePage.jsx
+++ frontend/src/profilePage/ProfilePage.jsx
@@ -22,63 +22,69 @@ function ProfilePage(props) {
   const { email } = props;
 
   console.log(userContext);
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    function handleClick() {
-        navigate('/editprofile');
-    }
-
-    return (
-    <>
-    <Image w={150} p={3} ml={3} src='../../assets/SnoozeLogo.svg'></Image>
-
-    <Box bg='mintgreen' p={4} align='center'>
-        <Image
-        borderRadius='full'
-        boxSize='130px'
-        src={image}
-        alt=''
-        />
-        {/* have to make onclick route to editprofile */}
-        <Button onClick={handleClick} mt={4} colorScheme='gray'>
-            Edit Profile 
-        </Button>
-    </Box>
-      
-    <Heading ml={20} mt={10} size="lg">Personal Info</Heading>
-        <Box p={4} ml={40} mt={4} fontSize="2xl" fontWeight={500}>
-            <Text>Name: {name}</Text>
-            <Text mt={5}>Email: {props.user.email}</Text>
-        </Box>
-
-    <Heading ml={20} mt={12} size="lg">Current Bookings</Heading>
-    <Box bg='indiepink' mt={5} ml={40} w='55em' h='20em'>
-        {/* have to input bookings data */}
-    </Box>
-
-    <Heading ml={20} mt={12} size="lg">Past Bookings</Heading>
-    <Box bg='indiepink' mt={5} ml={40} w='55em' h='20em'>
-        {/* have to input bookings data */}
-    </Box>
-
-    <Heading ml={20} mt={12} size="lg">Rewards</Heading>
-    <Box bg='darkGray' mt={5} ml={40} w='35em' h='40em'>
-        {/* have to figure out how to get reward points */}
-        <Flex justifyContent='center' alignItems='center'>
-        <Circle mt={10} size='20em' borderColor='pink' borderWidth='12px' color='black'>
-            <Text textAlign='center' fontWeight='bold' fontSize={22}>100000<br/>Rewards points</Text>
-        </Circle>
-        </Flex>
-
-        
-        
-    </Box>
-
-        
-        
-      
-    </>
-    );
+  function handleClick() {
+    navigate("/editprofile");
   }
-  
-  export default ProfilePage;
+
+  return (
+    <>
+      <Image w={150} p={3} ml={3} src="../../assets/SnoozeLogo.svg"></Image>
+
+      <Box bg="mintgreen" p={4} align="center">
+        <Image borderRadius="full" boxSize="130px" src={image} alt="" />
+        {/* have to make onclick route to editprofile */}
+        <Button onClick={handleClick} mt={4} colorScheme="gray">
+          Edit Profile
+        </Button>
+      </Box>
+
+      <Heading ml={20} mt={10} size="lg">
+        Personal Info
+      </Heading>
+      <Box p={4} ml={40} mt={4} fontSize="2xl" fontWeight={500}>
+        <Text>Name: {name}</Text>
+        <Text mt={5}>Email: {props.user.email}</Text>
+      </Box>
+
+      <Heading ml={20} mt={12} size="lg">
+        Current Bookings
+      </Heading>
+      <Box bg="indiepink" mt={5} ml={40} w="55em" h="20em">
+        {/* have to input bookings data */}
+      </Box>
+
+      <Heading ml={20} mt={12} size="lg">
+        Past Bookings
+      </Heading>
+      <Box bg="indiepink" mt={5} ml={40} w="55em" h="20em">
+        {/* have to input bookings data */}
+      </Box>
+
+      <Heading ml={20} mt={12} size="lg">
+        Rewards
+      </Heading>
+      <Box bg="darkGray" mt={5} ml={40} w="35em" h="40em">
+        {/* have to figure out how to get reward points */}
+        <Flex justifyContent="center" alignItems="center">
+          <Circle
+            mt={10}
+            size="20em"
+            borderColor="pink"
+            borderWidth="12px"
+            color="black"
+          >
+            <Text textAlign="center" fontWeight="bold" fontSize={22}>
+              100000
+              <br />
+              Rewards points
+            </Text>
+          </Circle>
+        </Flex>
+      </Box>
+    </>
+  );
+}
+
+export default ProfilePage;
