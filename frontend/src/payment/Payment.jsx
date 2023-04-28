bf490765539fb9887fef572ad84aaad8362fc9aa
cassidychu <cassidychu@yahoo.com>
2023-04-28 02:24:11 -0700

    payments

--- frontend/src/payment/Payment.jsx
+++ frontend/src/payment/Payment.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useEffect } from "react";
 import "./Payment.css";
 import { useNavigate } from "react-router-dom";
 import {
@@ -13,10 +13,11 @@ import {
   Button,
   Divider,
   HStack,
+  propNames,
 } from "@chakra-ui/react";
 import SnoozeHeader from "../general/SnoozeHeader";
 
-export default function Payment(props) {
+export default function Payment({ room, checkIn, checkOut, hotel, price }) {
   const firstNameRef = useRef();
   const lastNameRef = useRef();
   const cardNumberRef = useRef();
@@ -48,99 +49,104 @@ export default function Payment(props) {
     );
   };
 
-  return (
-    <>
-      <SnoozeHeader />
+  useEffect(() => {
+    console.log(room);
+    console.log(checkIn);
+    console.log(checkOut);
+    console.log(hotel);
+    console.log(price);
+  }, []);
 
-      <Flex
-        my={20}
-        w={"100%"}
+  return (
+    <Flex
+      my={20}
+      w={"100%"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      flexDirection={"column"}
+      gap={8}
+    >
+      <div>{price}</div>
+      <VStack
+        w={"25%"}
+        p={10}
         justifyContent={"center"}
         alignItems={"center"}
-        flexDirection={"column"}
-        gap={8}
+        gap={2}
+        border={"5px solid #9A9A9A"}
+        borderRadius={30}
       >
-        <VStack
-          w={"25%"}
-          p={10}
-          justifyContent={"center"}
-          alignItems={"center"}
-          gap={2}
-          border={"5px solid #9A9A9A"}
-          borderRadius={30}
-        >
-          <Heading textColor="#33333">Payment Details</Heading>
-          <FormControl isRequired>
-            <FormLabel m={0}>First Name</FormLabel>
-            <Input ref={firstNameRef} type="text" />
-          </FormControl>
-          <FormControl isRequired>
-            <FormLabel m={0}>Last Name</FormLabel>
-            <Input ref={lastNameRef} type="text" />
-          </FormControl>
-          <FormControl isRequired>
-            <FormLabel m={0}>Card Number</FormLabel>
-            <Input ref={cardNumberRef} type="number" />
-          </FormControl>
-          <FormControl isRequired>
-            <FormLabel m={0}>Expiration Date</FormLabel>
-            <Input ref={expDateRef} w={"50%"} placeholder="MM" type="month" />
-          </FormControl>
-          <FormControl isRequired>
-            <FormLabel m={0}>Security Code</FormLabel>
-            <Input ref={secCodeRef} w={"35%"} placeholder="123" type="number" />
-          </FormControl>
-        </VStack>
-        <Divider orientation="horizontal" w={3} />
-        <VStack
-          w={"25%"}
-          p={10}
-          justifyContent={"center"}
-          alignItems={"center"}
-          gap={2}
-          border={"5px solid #9A9A9A"}
-          borderRadius={30}
-        >
-          <Heading textColor="#33333">Billing Address</Heading>
-          <FormControl isRequired>
-            <FormLabel m={0}>Address</FormLabel>
-            <Input ref={addressOneRef} placeholder="Address Line 1" />
-            <Input ref={addressTwoRef} top={2} placeholder="Address Line 2" />
-          </FormControl>
-          <FormControl isRequired>
-            <FormLabel m={0}>City</FormLabel>
-            <Input ref={cityRef} placeholder="City" />
-          </FormControl>
-          <FormControl isRequired>
-            <FormLabel m={0}>State</FormLabel>
-            <Input ref={stateRef} placeholder="State" />
-          </FormControl>
-          <FormControl isRequired>
-            <FormLabel m={0}>Zip Code</FormLabel>
-            <Input ref={zipCodeRef} placeholder="Zip Code" />
-          </FormControl>
-          <FormControl isRequired>
-            <FormLabel m={0}>Country</FormLabel>
-            <Input ref={countryRef} placeholder="Country" />
-          </FormControl>
-        </VStack>
+        <Heading textColor="#33333">Payment Details</Heading>
+        <FormControl isRequired>
+          <FormLabel m={0}>First Name</FormLabel>
+          <Input ref={firstNameRef} type="text" />
+        </FormControl>
+        <FormControl isRequired>
+          <FormLabel m={0}>Last Name</FormLabel>
+          <Input ref={lastNameRef} type="text" />
+        </FormControl>
+        <FormControl isRequired>
+          <FormLabel m={0}>Card Number</FormLabel>
+          <Input ref={cardNumberRef} type="number" />
+        </FormControl>
+        <FormControl isRequired>
+          <FormLabel m={0}>Expiration Date</FormLabel>
+          <Input ref={expDateRef} w={"50%"} placeholder="MM" type="month" />
+        </FormControl>
+        <FormControl isRequired>
+          <FormLabel m={0}>Security Code</FormLabel>
+          <Input ref={secCodeRef} w={"35%"} placeholder="123" type="number" />
+        </FormControl>
+      </VStack>
+      <Divider orientation="horizontal" w={3} />
+      <VStack
+        w={"25%"}
+        p={10}
+        justifyContent={"center"}
+        alignItems={"center"}
+        gap={2}
+        border={"5px solid #9A9A9A"}
+        borderRadius={30}
+      >
+        <Heading textColor="#33333">Billing Address</Heading>
+        <FormControl isRequired>
+          <FormLabel m={0}>Address</FormLabel>
+          <Input ref={addressOneRef} placeholder="Address Line 1" />
+          <Input ref={addressTwoRef} top={2} placeholder="Address Line 2" />
+        </FormControl>
+        <FormControl isRequired>
+          <FormLabel m={0}>City</FormLabel>
+          <Input ref={cityRef} placeholder="City" />
+        </FormControl>
+        <FormControl isRequired>
+          <FormLabel m={0}>State</FormLabel>
+          <Input ref={stateRef} placeholder="State" />
+        </FormControl>
+        <FormControl isRequired>
+          <FormLabel m={0}>Zip Code</FormLabel>
+          <Input ref={zipCodeRef} placeholder="Zip Code" />
+        </FormControl>
+        <FormControl isRequired>
+          <FormLabel m={0}>Country</FormLabel>
+          <Input ref={countryRef} placeholder="Country" />
+        </FormControl>
+      </VStack>
 
-        <HStack gap={4}>
-          <Button size="lg" backgroundColor="gr" top={10}>
-            Back
-          </Button>
+      <HStack gap={4}>
+        <Button size="lg" backgroundColor="gr" top={10}>
+          Back
+        </Button>
 
-          <Button
-            onClick={handleSubmit}
-            backgroundColor="#c6c1dc"
-            textColor="white"
-            top={10}
-            size="lg"
-          >
-            Confirm Booking
-          </Button>
-        </HStack>
-      </Flex>
-    </>
+        <Button
+          onClick={handleSubmit}
+          backgroundColor="#c6c1dc"
+          textColor="white"
+          top={10}
+          size="lg"
+        >
+          Confirm Booking
+        </Button>
+      </HStack>
+    </Flex>
   );
 }
